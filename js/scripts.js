21a4f346ba0fc722ee2bcf90e31f7b794a12da89
Reed-Carter <114263922+Reed-Carter@users.noreply.github.com>
2022-10-17 15:59:16 -0800

    delete extra window alerts

--- js/scripts.js
+++ js/scripts.js
@@ -20,11 +20,6 @@ function divide(number1, number2) {
 const number1 = parseInt(prompt("Enter a number"));
 const number2 = parseInt(prompt("Enter another number"));
 
-window.alert("The sum equals " + add(number1, number2));
-window.alert("The difference equals " + subtract(number1, number2));
-window.alert("The product equals " + multiply(number1, number2));
-window.alert("The quotient equals " + divide(number1, number2));
-
 let addition = number1 + "+" + number2 + "=" + add(number1, number2) + "\n";
 let subtraction = number1 + "-" + number2 + "=" + subtract(number1, number2) + "\n";
 let multiplication = number1 + "*" + number2 + "=" + multiply(number1, number2) + "\n";
