b0694ecbef1980f90de10a23597ef3216330a6a8
Reed-Carter <114263922+Reed-Carter@users.noreply.github.com>
2022-10-17 15:55:41 -0800

    alert all functions to display at once

--- js/scripts.js
+++ js/scripts.js
@@ -25,4 +25,9 @@ window.alert("The difference equals " + subtract(number1, number2));
 window.alert("The product equals " + multiply(number1, number2));
 window.alert("The quotient equals " + divide(number1, number2));
 
-window.alert(number1, " + ",number2, " = "+ add(number1, number2))
+let addition = number1 + "+" + number2 + "=" + add(number1, number2) + "\n";
+let subtraction = number1 + "-" + number2 + "=" + subtract(number1, number2) + "\n";
+let multiplication = number1 + "*" + number2 + "=" + multiply(number1, number2) + "\n";
+let division = number1 + "/" + number2 + "=" + divide(number1, number2) + "\n";
+
+window.alert(addition + subtraction + multiplication + division)
