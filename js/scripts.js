de8188c6610c822a2b53a133cc0bb7036770342d
Reed-Carter <114263922+Reed-Carter@users.noreply.github.com>
2022-10-17 15:25:39 -0800

    add substraction, multiplication, and divition capabilities to the calculator

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,26 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
+function subtract(number1, number2) {
+  return number1 - number2;
+}
 
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
 
 
 // user interface logic
 const number1 = parseInt(prompt("Enter a number"));
 const number2 = parseInt(prompt("Enter another number"));
 
-window.alert(add(number1, number2));
+window.alert("The sum equals " + add(number1, number2));
+window.alert("The difference equals " + subtract(number1, number2));
+window.alert("The product equals " + multiply(number1, number2));
+window.alert("The quotient equals " + divide(number1, number2));
+
+window.alert(number1, " + ",number2, " = "+ add(number1, number2))
